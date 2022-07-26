66fdde1ce5f81242ca1e46c97cabb58aad76cbe0
Achhada Hiren Rajkumar <achhadahiren201@gmail.com>
2022-07-27 01:40:38 +0530

    calculator

--- 2101CS03_LAB1/calculator.c
+++ 2101CS03_LAB1/calculator.c
@@ -4,6 +4,7 @@ int main()
     int option;
     printf("1. To add two numbers\n");
     printf("2. To add two numbers\n");
+    printf("3. To add two numbers\n");
     scanf("%d", &option);
     if(option==1)
     {
@@ -17,7 +18,7 @@ int main()
     }
     if(option==2)
     {
-        // code for adding two numbers
+        // code for subtraction two numbers
         int num1,num2;
         printf("Enter two numbers: ");
         scanf("%d",&num1);
@@ -25,5 +26,16 @@ int main()
         printf("The value of difference is%d\n", num1 - num2);
         return 0;
     }
+    if(option==3)
+    {
+        // code for multiplication two numbers
+        int num1,num2;
+        printf("Enter two numbers: ");
+        scanf("%d",&num1);
+        scanf("%d",&num2);
+        printf("The value of multiplication is%d\n", num1*num2);
+        return 0;
+    }
+
 
 }
