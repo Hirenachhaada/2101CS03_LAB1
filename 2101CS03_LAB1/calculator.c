a84d9b9b06f3626891aa4638633e5db5a241ad1a
Achhada Hiren Rajkumar <achhadahiren201@gmail.com>
2022-07-27 01:28:14 +0530

    adding subtraction code

--- 2101CS03_LAB1/calculator.c
+++ 2101CS03_LAB1/calculator.c
@@ -3,6 +3,7 @@ int main()
 {
     int option;
     printf("1. To add two numbers\n");
+    printf("2. To add two numbers\n");
     scanf("%d", &option);
     if(option==1)
     {
@@ -11,7 +12,17 @@ int main()
         printf("Enter two numbers: ");
         scanf("%d",&num1);
         scanf("%d",&num2);
-        printf("%d\n", num1 + num2);
+        printf("The value of sum is%d\n", num1 + num2);
+        return 0;
+    }
+    if(option==2)
+    {
+        // code for adding two numbers
+        int num1,num2;
+        printf("Enter two numbers: ");
+        scanf("%d",&num1);
+        scanf("%d",&num2);
+        printf("The value of difference is%d\n", num1 - num2);
         return 0;
     }
 
