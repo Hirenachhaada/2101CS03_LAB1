907403a4cdb42d167dcd97f34409224e344382c0
Achhada Hiren Rajkumar <achhadahiren201@gmail.com>
2022-07-27 11:21:05 +0530

    insertion sorting

--- 2101CS03_LAB1/insert_sort.c
+++ 2101CS03_LAB1/insert_sort.c
@@ -4,39 +4,39 @@
 /* Function to sort an array using insertion sort*/
 void insertionSort(int arr[], int n)
 {
-	int i, key, j;
-	for (i = 1; i < n; i++) {
-		key = arr[i];
-		j = i - 1;
-
-		/* Move elements of arr[0..i-1], that are
-		greater than key, to one position ahead
-		of their current position */
-		while (j >= 0 && arr[j] > key) {
-			arr[j + 1] = arr[j];
-			j = j - 1;
-		}
-		arr[j + 1] = key;
-	}
+    int i, k, j;
+    for (i = 1; i < n; i++)
+    {
+        k = arr[i];
+        j = i - 1;
+        while ( arr[j] > k && j >= 0)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = k;
+    }
 }
 
-// A utility function to print an array of size n
-void printArray(int arr[], int n)
-{
-	int i;
-	for (i = 0; i < n; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
-}
 
-/* Driver program to test insertion sort */
+
 int main()
 {
-	int arr[] = { 12, 11, 13, 5, 6 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-
-	insertionSort(arr, n);
-	printArray(arr, n);
-
-	return 0;
+        int n;
+        printf("Enter the number of elements in the array: ");
+        scanf("%d", &n);
+        int arr[n];
+        printf("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        insertionSort(arr, n);
+        printf("The sorted array is: ");
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+		return 0;
 }
